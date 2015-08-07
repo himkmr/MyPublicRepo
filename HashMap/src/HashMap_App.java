07073abd58a353066fdc05b3a0371919b9e06e53
himanshu <himkmr@gmail.com>
2015-08-07 15:38:24 -0400

    with files

--- HashMap/src/HashMap_App.java
+++ HashMap/src/HashMap_App.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -5,9 +8,22 @@ import java.util.Scanner;
 public class HashMap_App {
 public static void main(String[] ar)
 {
+	
 	HashMap<Integer, String> myMap = new HashMap<Integer, String>();
 
 	Scanner sc = new Scanner(System.in);
+	//write to file
+		System.out.println("enter file name: ");
+		String file = sc.nextLine();
+		String filename = (System.getProperty("user.dir") + File.separatorChar +file);
+		PrintWriter writer= null;
+		try {
+			writer = new PrintWriter(filename);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	while(true)
 	{
 		System.out.println("Enter Integer: ");
@@ -16,7 +32,7 @@ public static void main(String[] ar)
 		sc.nextLine();	//trash 
 		String value = sc.nextLine();
 		myMap.put(key, value);
-		
+		writer.println(key+"   "+value);
 		System.out.println("Enter more? (y/n)");
 		String response = sc.nextLine();
 		if(!response.equalsIgnoreCase("y"))
@@ -34,6 +50,7 @@ public static void main(String[] ar)
 			System.out.println("Key not found, please enter value to add");
 			String value = sc.nextLine();
 			myMap.put(key, value);		
+			writer.println(key+"   "+value);
 		}
   		System.out.println("key: "+ key + "       Value: " +myMap.get(key));
 		System.out.println(" more? (y/n)");
@@ -43,6 +60,8 @@ public static void main(String[] ar)
 	
 	}
 	
+	
+	writer.close();
 	sc.close();
 	
 	
